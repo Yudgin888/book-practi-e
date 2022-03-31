4ebe91be3e16a41bdc6a7ee75ae78168c7329b76
Your Name <you@example.com>
2022-03-31 17:45:26 +0300

    autogenerator

---
+++ public_html/src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/')]
+    public function index(Request $request): Response
+    {
+        dump($request);
+        return new Response('<body>test</body>');
+    }
+}
